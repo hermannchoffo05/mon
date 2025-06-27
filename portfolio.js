120c000197c49c9876b0297463b01d1b707e1fea
hermannchoffo05 <hermannchoffo05@gmail.com>
2025-06-27 16:21:08 +0200

    mon premier commit

---
+++ portfolio.js
@@ -0,0 +1,45 @@
+    const text = "Bienvenue je suis CHOFFO Hermann"
+    let i=0;
+    function typewriter(){
+        if(i<text.length){
+            document.getElementById("writer").innerHTML+= text.charAt(i);
+            i++;
+            setTimeout(typewriter,70);
+        }
+    }
+    window.onload = typewriter; 
+     
+     
+     const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+        // Burger Animation
+        burger.classList.toggle('toggle');
+    });
+
+    // Fermer le menu quand un lien est cliqué (pour les mobiles)
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            nav.classList.remove('nav-active');
+            burger.classList.remove('toggle');
+            navLinks.forEach(item => item.style.animation = ''); // Réinitialiser l'animation pour permettre de la rejouer
+        });
+    });
+}
+
+navSlide();
